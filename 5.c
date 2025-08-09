eb7ad526f9cddecefd891bf2e7e1a101f496cb7a
machadopng <jorgemagalhaesmachado@gmail.com>
2025-08-09 11:28:41 -0300

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,19 @@
+programa
+{
+	
+	funcao inicio()
+	{
+          inteiro n
+          inteiro i
+
+          escreva("Me dê o valor de \"n\"\n")
+          leia(n)
+
+          para(i = n; i >= 1; i--)
+          {
+          	escreva(i)
+          }
+          escreva("\n\n\n")
+          
+	}
+}
